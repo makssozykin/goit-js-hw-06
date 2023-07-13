fdabaf17025caafcda52990696b280388d46bc5d
makssozykin <mvsozykin@gmail.com>
2023-07-13 17:35:23 +0300

    HomeworkJSmodule6_task3

--- js/task-03.js
+++ js/task-03.js
@@ -12,3 +12,26 @@ const images = [
     alt: 'Group of Horses Running',
   },
 ];
+
+const list = document.querySelector(".gallery");
+list.style.display = "flex";
+list.style.gap = "20px";
+list.style.marginLeft = "auto";
+list.style.marginRight = "auto";
+
+
+const markup = images.map(image => `<li class="item"><img class="img-item" src="${image.url}" alt="${image.alt}"></li>`).join("");
+
+list.insertAdjacentHTML("beforeend", markup);
+
+const liElements = document.querySelectorAll(".item");
+for (const element of liElements) {
+  element.style.width = "200px";
+  element.style.height = "150px";
+  element.style.listStyle = "none";
+  element.style.borderColor = "red";
+  element.style.borderStyle = "dashed";
+  element.style.borderWidth = "2px";
+  element.firstElementChild.style.width = "100%";
+  element.firstElementChild.style.height = "100%";
+}

8e69ec9f45f02cfcefff3ed16f331d38801d4e6a
makssozykin <mvsozykin@gmail.com>
2023-07-14 18:30:10 +0300

    HomeworkJSmodule6_task8

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,17 @@
+const form = document.querySelector('.login-form');
+const Alert = "Всі поля повинні бути заповнені";
 
+const onSubmit = (e) => { 
+    e.preventDefault();
+    if (form.elements.email.value === "" || form.elements.password.value === "") { 
+        alert(Alert);
+    }
+    const userData = {
+        Email: form.elements.email.value,
+        Password: form.elements.password.value
+    }
+    console.log(userData);
+    form.reset();
+}
+
+form.addEventListener("submit", onSubmit);
